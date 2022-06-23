79b57643171da226aac02cdae7c35aaa72499d37
AnneKammenhed <anne.juriado@gmail.com>
2022-06-23 07:30:21 +0000

    add structure and welcome scentence

--- run.py
+++ run.py
@@ -1,2 +1,27 @@
+# google api info
 
-print("hej is hello in Swedish")
+
+
+def collect_data():
+    """
+    Function to collect data about bills for 
+    rent, electricity and telephone from user.
+    """
+    
+    print(f"Welcome to Annes monthly cost calculator!")
+
+# check if data is valid print error if not
+
+# add cost to worksheet
+
+# calculate sum
+
+# add sum to worksheet
+
+# return sum to user
+
+# calculate average
+
+# return average to user
+
+collect_data()
