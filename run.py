da6b5f6454377d397d2625d0535fcf9e3d08c6d2
AnneKammenhed <anne.juriado@gmail.com>
2022-06-23 08:35:05 +0000

    add credentials to google worksheet

--- run.py
+++ run.py
@@ -1,5 +1,17 @@
 # google api info
+import gspread
+from google.oauth2.service_account import Credentials
 
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file("creds.json")
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open("monthly_bills")
 
 
 def collect_data():
@@ -9,6 +21,11 @@ def collect_data():
     """
     
     print(f"Welcome to Annes monthly cost calculator!")
+
+
+bills = SHEET.worksheet("bills")
+data = bills.get_all_values()
+print(data)
 
 # check if data is valid print error if not
 
